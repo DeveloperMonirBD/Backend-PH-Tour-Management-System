03ba3aaac0874e1456d833f162c48245b42c35e1
Monir <mrmonir0558@gmail.com>
2025-07-18 01:04:36 +0600

    configuring passportjs for backend and google authentication configuration

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -22,7 +22,7 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response, next: Ne
     //     httpOnly: true,
     //     secure: false
     // })
-    
+
     //set accessToken and refreshToken in the cookie
     setAuthCookie(res, loginInfo);
 
@@ -61,18 +61,17 @@ const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: N
 
 // logout
 const logout = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
-    res.clearCookie("accessToken", {
+    res.clearCookie('accessToken', {
         httpOnly: true,
         secure: false,
-        sameSite: "lax"
-    })
+        sameSite: 'lax'
+    });
 
-    res.clearCookie("refreshToken", {
+    res.clearCookie('refreshToken', {
         httpOnly: true,
         secure: false,
-        sameSite: "lax"
-    })
+        sameSite: 'lax'
+    });
 
     sendResponse(res, {
         success: true,
@@ -82,8 +81,25 @@ const logout = catchAsync(async (req: Request, res: Response, next: NextFunction
     });
 });
 
+// reset Password
+const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const newPassword = req.body.newPassword;
+    const oldPassword = req.body.oldPassword;
+    const decodedToken = req.user;
+
+    await AuthServices.resetPassword(oldPassword, newPassword, decodedToken);
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'Password Changed Successfully',
+        data: null
+    });
+});
+
 export const AuthControllers = {
     credentialsLogin,
     getNewAccessToken,
-    logout
+    logout,
+    resetPassword
 };
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,9 +1,12 @@
-import  httpStatus  from 'http-status-codes';
-import { IUser } from "../user/user.interface"
-import { User } from "../user/user.model";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import bcryptjs from 'bcryptjs';
+import httpStatus from 'http-status-codes';
+import { JwtPayload } from 'jsonwebtoken';
+import { envVars } from '../../config/env';
 import AppError from '../../errorHelpers/appError';
-import bcryptjs from "bcryptjs";
 import { createNewAccessTokenWithRefreshToken, createUserTokens } from '../../utils/userToken';
+import { IUser } from '../user/user.interface';
+import { User } from '../user/user.model';
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
     const { email, password } = payload;
@@ -13,7 +16,7 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
     if (!isUserExist) {
         throw new AppError(httpStatus.BAD_REQUEST, 'Email does not exist');
     }
-    const isPasswordMatch = await bcryptjs.compare(password as string, isUserExist.password as string)
+    const isPasswordMatch = await bcryptjs.compare(password as string, isUserExist.password as string);
 
     if (!isPasswordMatch) {
         throw new AppError(httpStatus.BAD_REQUEST, 'Incorrect password');
@@ -27,31 +30,45 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
     // const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
     // const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES)
 
-    const userTokens = createUserTokens(isUserExist)
+    const userTokens = createUserTokens(isUserExist);
 
     // delete isUserExist.password;
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const {password: pass, ...rest} = isUserExist.toObject()
+    const { password: pass, ...rest } = isUserExist.toObject();
 
     return {
-        accessToken : userTokens.accessToken,
-        refreshToken : userTokens.refreshToken,
+        accessToken: userTokens.accessToken,
+        refreshToken: userTokens.refreshToken,
         user: rest
-    }
-}
+    };
+};
 
-const getNewAccessToken= async (refreashToken: string) => {
+const getNewAccessToken = async (refreashToken: string) => {
     const newAccessToken = await createNewAccessTokenWithRefreshToken(refreashToken);
 
     return {
         accessToken: newAccessToken
     };
-}
+};
+
+const resetPassword = async (oldPassword: string, newPassword: string, decodedToken: JwtPayload) => {
+    const user = await User.findById(decodedToken.userId);
+
+    const isOldPasswordMatch = await bcryptjs.compare(oldPassword, user?.password as string);
+    if (!isOldPasswordMatch) {
+        throw new AppError(httpStatus.UNAUTHORIZED, 'Old Password does not match');
+    }
+
+    user!.password = await bcryptjs.hash(newPassword, Number(envVars.BCRYPT_SALT_ROUND));
+    user!.save();
+    
+};
 
 //user - login - token (email, role, _id) - booking / payment / booking / payment cancel -
 
 export const AuthServices = {
     credentialsLogin,
-    getNewAccessToken
+    getNewAccessToken,
+    resetPassword
 };
