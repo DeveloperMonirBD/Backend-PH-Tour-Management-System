56130536fc653bbaf7b1f79fc42f94769a2c0493
Monir <mrmonir0558@gmail.com>
2025-07-19 23:45:05 +0600

    Created interface and model for Division and interface for Tour Types

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,16 +3,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { envVars } from '../config/env';
 import AppError from '../errorHelpers/appError';
-import { handleValidationError } from '../helpers/handleValidationError';
-import { handleZodError } from '../helpers/handleZodError';
 import { handleCastError } from '../helpers/handleCastError';
 import { handleDuplicateError } from '../helpers/handleDuplicateError';
+import { handleValidationError } from '../helpers/handleValidationError';
+import { handleZodError } from '../helpers/handleZodError';
 import { TErrorSources } from '../interfaces/error.types';
 
-
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    
-    if (envVars.NODE_ENV === "development") {
+    if (envVars.NODE_ENV === 'development') {
         console.log(err);
     }
 
@@ -29,28 +27,27 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
     }
     // Object ID error / Cast Error
     else if (err.name === 'CastError') {
-        const simplifiedError = handleCastError(err)
+        const simplifiedError = handleCastError(err);
         statusCode = simplifiedError.statusCode;
         message = simplifiedError.message;
-    }
-    else if (err instanceof AppError) {
+    } else if (err instanceof AppError) {
         statusCode = err.statusCode;
         message = err.message;
     }
     // Zod error
     else if (err.name === 'ZodError') {
-        const simplifiedError = handleZodError(err)
+        const simplifiedError = handleZodError(err);
         statusCode = simplifiedError.statusCode;
         message = simplifiedError.message;
-        errorSources = simplifiedError.errorSources as TErrorSources[]
+        errorSources = simplifiedError.errorSources as TErrorSources[];
     }
 
     //Mongoose Validation Error
     else if (err.name === 'ValidationError') {
-        const simplifiedError = handleValidationError(err)
+        const simplifiedError = handleValidationError(err);
         statusCode = simplifiedError.statusCode;
-        errorSources = simplifiedError.errorSources as TErrorSources[]
-        message = simplifiedError.message
+        errorSources = simplifiedError.errorSources as TErrorSources[];
+        message = simplifiedError.message;
     } else if (err instanceof Error) {
         statusCode = 500;
         message = err.message;
@@ -60,7 +57,7 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
         success: false,
         message,
         errorSources,
-        err: envVars.NODE_ENV === "development" ? err : null,
+        err: envVars.NODE_ENV === 'development' ? err : null,
         stack: envVars.NODE_ENV === 'development' ? err.stack : null
     });
 };
