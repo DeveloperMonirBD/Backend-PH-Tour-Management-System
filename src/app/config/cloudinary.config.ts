eec334a30ed7b092cd00d91e37136015866b699b
Monir <mrmonir0558@gmail.com>
2025-07-26 22:11:29 +0600

    configure Multer and Cloudinary for image Upload

---
+++ src/app/config/cloudinary.config.ts
@@ -0,0 +1,14 @@
+// Frontend ->  Form Data with image file ->  Multer ->  Form data ->  Req (Body + File)
+
+import { v2 as cloudinary } from 'cloudinary';
+import { envVars } from './env';
+
+// Amader Folder ->  image ->  form data ->  file -> Multer ->  Nijer akta folder(temporary) -> Req.file
+
+// req.file -> cloudinary(req.file) -> mongoose -> mongodb
+
+cloudinary.config({
+    cloud_name: envVars.CLOUDINARY.CLOUDINARY_CLOUD_NAME,
+    api_key: envVars.CLOUDINARY.CLOUDINARY_API_KEY,
+    api_secret: envVars.CLOUDINARY.CLOUDINARY_API_SECRET
+});
